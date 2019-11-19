0bfbe51a8b5702048eb77708e82f3c4b1bd36e15
FireEmerald <fire.github@gmail.com>
2019-11-19 08:31:43 +0100

    Moved ApiAdapterTests class to it's expected directory

--- source/Test.IoC.SampleApp/OnlineService/ApiAdapterTests.cs
+++ source/Test.IoC.SampleApp/OnlineService/ApiAdapterTests.cs
@@ -3,24 +3,24 @@ using IoC.SampleApp.OnlineService.WebClient;
 using Moq;
 using NUnit.Framework;
 
-namespace Test.IoC.SampleApp
+namespace Test.IoC.SampleApp.OnlineService
 {
     public class ApiAdapterTests
     {
+        // Advanced test setup using TestBase component
         private TestBase TestBase;
 
         [SetUp]
         public void Setup()
         {
+            // Called before each TestCase
             TestBase = new TestBase();
         }
 
         [TestCase(ApiStatus.Ok, "{ status: success }")]
         [TestCase(ApiStatus.ClientError, "")]
-        public void Test__GetServiceStatus(ApiStatus expectedStatus, string url)
+        public void Test__WebClient__GetServiceStatus(ApiStatus expectedStatus, string url)
         {
-            TestContext.WriteLine($"Executing test '{url}'.");
-
             // Arrange
             var mockWebClient = new Mock<IWebClient>();
             mockWebClient.Setup(x => x.Request(It.IsAny<string>())).Returns(url);
